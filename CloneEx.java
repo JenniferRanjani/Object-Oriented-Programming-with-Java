64f6376a1cbad5357b9bdf189f58c0636a0e8227
JenniferRanjani <43373794+JenniferRanjani@users.noreply.github.com>
2018-10-23 16:57:53 +0530

    Example for Shallow \ Deep Cloning

---
+++ CloneEx.java
@@ -0,0 +1,42 @@
+class Test1 
+{ 
+    int x, y; 
+} 
+  
+class Test2 implements Cloneable 
+{ 
+    int a, b; 
+  
+    Test1 c = new Test1(); 
+  
+    public Object clone() throws
+                CloneNotSupportedException 
+    {  
+	Test2 t = (Test2)super.clone();
+	//t.c = new Test1();   //uncomment it for deep copy
+        return t; 
+    } 
+} 
+  
+class CloneEx
+{ 
+    public static void main(String args[]) throws
+                             CloneNotSupportedException 
+    { 
+       Test2 t1 = new Test2(); 
+       t1.a = 10; 
+       t1.b = 20; 
+       t1.c.x = 30; 
+       t1.c.y = 40; 
+  
+       Test2 t3 = (Test2)t1.clone(); 
+       t3.a = 100; 
+       t3.b = 200;
+       t3.c.x = 300; 
+  
+       System.out.println(t1.a + " " + t1.b + " " + 
+                          t1.c.x + " " + t1.c.y); 
+       System.out.println(t3.a + " " + t3.b + " " + 
+                          t3.c.x + " " + t3.c.y); 
+    } 
+} 
