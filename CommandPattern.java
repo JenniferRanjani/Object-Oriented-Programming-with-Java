f976de81a5b38bda7da6b50f75e34ab8d46a4484
JenniferRanjani <43373794+JenniferRanjani@users.noreply.github.com>
2018-11-24 13:33:25 +0530

    Example to demonstrate Command Design Pattern

---
+++ CommandPattern.java
@@ -0,0 +1,84 @@
+//Code Courtesy: https://www.tutorialspoint.com/design_pattern/command_pattern.htm
+
+import java.util.*;
+
+ interface Order {
+//Command
+   void execute();
+}
+
+
+class Stock {
+	
+   private String name = "ABC";
+   private int quantity = 10;
+
+   public void buy(){
+      System.out.println("Stock [ Name: "+name+", Quantity: " + quantity +" ] bought");
+   }
+   public void sell(){
+      System.out.println("Stock [ Name: "+name+", Quantity: " + quantity +" ] sold");
+   }
+}
+
+
+class BuyStock implements Order {
+   private Stock abcStock;
+
+   public BuyStock(Stock abcStock){
+      this.abcStock = abcStock;
+   }
+
+   public void execute() {
+      abcStock.buy();
+   }
+}
+
+class SellStock implements Order {
+   private Stock abcStock;
+
+   public SellStock(Stock abcStock){
+      this.abcStock = abcStock;
+   }
+
+   public void execute() {
+      abcStock.sell();
+   }
+}
+
+
+
+  class Broker {
+//Invoker
+   private List<Order> orderList = new ArrayList<Order>(); 
+
+   public void takeOrder(Order order){
+      orderList.add(order);		
+   }
+
+   public void placeOrders(){
+   
+      for (Order order : orderList) {
+         order.execute();
+      }
+      orderList.clear();
+   }
+}
+
+
+public class CommandPattern {
+   public static void main(String[] args) {
+	   
+//Client
+      Stock abcStock = new Stock();
+
+      BuyStock buyStockOrder = new BuyStock(abcStock);
+      SellStock sellStockOrder = new SellStock(abcStock);
+
+      Broker broker = new Broker();
+      broker.takeOrder(buyStockOrder);
+      broker.takeOrder(sellStockOrder);
+
+      broker.placeOrders();
+   }
+}
