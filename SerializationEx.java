487855974242cc2b17c5787d912adc4ec043a8af
JenniferRanjani <43373794+JenniferRanjani@users.noreply.github.com>
2018-11-24 14:46:25 +0530

    Example to demonstrate serialization & transient

--- SerializationEx.java
+++ SerializationEx.java
@@ -3,7 +3,7 @@ import java.io.*;
 class SerializationEx implements Serializable  {
 	// Normal variables 
     int i = 20;
-    float j = 30; 
+    transient float j = 30; 
     
     public static void main(String[] args) throws Exception 
     { 
@@ -27,3 +27,29 @@ class SerializationEx implements Serializable  {
     } 
 
 }
+
+//Uncomment the following lines to see the impact of transient on writeObject and readObject
+// public class SerializationEx implements Serializable  {
+// // Normal variables 
+//     int i = 20;
+//     transient float j = 30; 
+    
+//     public static void main(String[] args) throws Exception 
+//     { 
+//     	SerializationEx input = new SerializationEx(); 
+  
+//         // serialization 
+//         FileOutputStream fos = new FileOutputStream("abc.txt"); 
+//         ObjectOutputStream oos = new ObjectOutputStream(fos); 
+//         oos.writeObject(input);
+//         oos.close();
+  
+//         // de-serialization 
+//         FileInputStream fis = new FileInputStream("abc.txt"); 
+//         ObjectInputStream ois = new ObjectInputStream(fis); 
+       
+//         SerializationEx output = (SerializationEx)ois.readObject();
+//         System.out.println("i ="+ output.i + "j ="+output.j);
+//         ois.close();
+//     } 
+// }
